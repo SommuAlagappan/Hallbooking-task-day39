36b871c2c1ec1fe589c5e36ca7e4834141327d1d
SommuAlagappan <sommualagappanm.1001@gmail.com>
2022-12-06 11:53:14 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,146 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongodb = require("mongodb");
+const mongoClient = mongodb.MongoClient;
+const dotenv = require('dotenv').config();
+const URL = process.env.DB;
+//middleware
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+const rooms = [];
+const customers = [];
+
+app.get("/", function (req, res) {
+  res.json({ message: "Success..." });
+});
+
+//get(display) rooms
+app.get("/rooms", async function (request, response) {
+  try {
+    // Open the connection
+    const connection = await mongoClient.connect(URL);
+    // Select DB
+    const db = connection.db("hallbooking");
+    // select the collection and do operation
+    let rooms = await db.collection("rooms").find().toArray();
+    // Close the connection
+    await connection.close();
+
+    response.json(rooms);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//get(display) customers
+app.get("/customers", async function (request, response) {
+  try {
+    // Open the connection
+    const connection = await mongoClient.connect(URL);
+    // Select DB
+    const db = connection.db("hallbooking");
+    // select the collection and do operation
+    let customers = await db.collection("customers").find().toArray();
+    // Close the connection
+    await connection.close();
+
+    response.json(customers);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//post(insert) rooms
+app.post("/room", async function (request, response) {
+  try {
+    // Open the connection
+    const connection = await mongoClient.connect(URL);
+    // Select DB
+    const db = connection.db("hallbooking");
+    // select the collection and do operation
+    await db.collection("rooms").insertOne(request.body);
+    // Close the connection
+    await connection.close();
+
+    response.json({
+      message: "Room added",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+//post(booking) rooms
+app.post("/bookroom", async function (request, response) {
+  try {
+    // Open the connection
+    const connection = await mongoClient.connect(URL);
+    // Select DB
+    const db = connection.db("hallbooking");
+    // select the collection and do operation
+    await db.collection("customers").insertOne(request.body);
+    // Close the connection
+    await connection.close();
+
+    response.json({
+      message: "Customers and booking details added successfully!!!",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  
+});
+
+//get roomsbooked
+app.get("/roomsbooked", async function (request, response) {
+  try {
+    const pipeline = [
+      {
+        $lookup: {
+          from: "customers",
+          localField: "_id",
+          foreignField: "roomID",
+          as: "customer",
+        },
+      },
+      {
+        $addFields: {
+          reservation: {
+            $cond: [
+              {
+                $eq: ["$customer", []],
+              },
+              "Not booked",
+              "Booked",
+            ],
+          },
+        },
+      },
+    ]
+    // Open the connection
+    const connection = await mongoClient.connect(URL);
+    // Select DB
+    const db = connection.db("hallbooking");
+    const result =await db
+      .collection("rooms")
+      .aggregate(pipeline)
+      .toArray();
+      
+    response.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//listen
+app.listen(process.env.PORT || 3005);
+
+
